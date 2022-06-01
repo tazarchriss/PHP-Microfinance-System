cf8d4868db9f09ade804fd5302fd7ea64325bee5
Baltazar Christian <77143206+tazarchriss@users.noreply.github.com>
2022-06-01 23:36:18 +0300

    Update addstaff.php

--- addstaff.php
+++ addstaff.php
@@ -1,5 +1,12 @@
 <!-- This is the page for adding a new staff -->
-<?php include 'include/header.php'; ?>
+<?php
+  session_start();
+  include 'config/connection.php';
+  if ($_SESSION['id']=='') {
+    header('Location:index.php');
+  }
+  else{
+       include 'include/header.php'; ?>
         <div id="layoutSidenav">
         <?php include 'include/adminnav.php'; ?>
             <div id="layoutSidenav_content">
@@ -20,13 +27,13 @@
                                             <div class="row mb-3">
                                                 <div class="col-md-6">
                                                     <div class="form-floating mb-3 mb-md-0">
-                                                        <input class="form-control" name="fname" id="inputFirstName" type="text" placeholder="Enter your first name" />
+                                                        <input class="form-control" name="fname" id="inputFirstName" type="text" required />
                                                         <label for="inputFirstName">First name</label>
                                                     </div>
                                                 </div>
                                                 <div class="col-md-6">
                                                     <div class="form-floating">
-                                                        <input class="form-control" name="mname" id="inputLastName" type="text" placeholder="Enter your last name" />
+                                                        <input class="form-control" name="mname" id="inputLastName" type="text" required />
                                                         <label for="inputLastName">Middle name</label>
                                                     </div>
                                                 </div>
@@ -34,13 +41,13 @@
                                             <div class="row mb-3">
                                                 <div class="col-md-6">
                                                     <div class="form-floating mb-3 mb-md-0">
-                                                        <input class="form-control" name="lname" id="inputFirstName" type="text" placeholder="Enter your first name" />
+                                                        <input class="form-control" name="lname" id="inputFirstName" type="text" required />
                                                         <label for="inputFirstName">Last name</label>
                                                     </div>
                                                 </div>
                                                 <div class="col-md-6">
                                                     <div class="form-floating">
-                                                        <input class="form-control" name="email" id="inputLastName" type="email" placeholder="Enter your last name" />
+                                                        <input class="form-control" name="email" id="inputLastName" type="email" required />
                                                         <label for="inputLastName">Email</label>
                                                     </div>
                                                 </div>
@@ -48,7 +55,7 @@
                                             <div class="row mb-3">
                                                 <div class="col-md-6">
                                                     <div class="form-floating mb-3 mb-md-0">
-                                                        <input class="form-control" name="pnumber" id="inputFirstName" type="text" placeholder="Enter your first name" />
+                                                        <input class="form-control" name="pnumber" id="inputFirstName" type="text" required />
                                                         <label for="inputFirstName">Phonenumber</label>
                                                     </div>
                                                 </div>
@@ -66,7 +73,7 @@
     
                                             <div class="mt-4 mb-0">
                                                 <div class="d-grid">
-                                                    <button type="submit" class="btn btn-primary btn-block" name="save">Save Staff</button>
+                                                    <button type="submit" class="btn btn-outline-primary btn-block" name="save"><i class="fa fa-save"></i> Save Staff</button>
                                                  
                                                 </div>
                                             </div>
@@ -75,4 +82,6 @@
                         </div>
                     </div>
                 </main>
-                <?php include 'include/footer.php'; ?>
+                <?php include 'include/footer.php'; 
+  }
+                ?>
