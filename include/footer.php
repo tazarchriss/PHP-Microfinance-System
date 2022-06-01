e021ae25fcd7d5c585fceb656d1ad9fb84e4dbc1
Baltazar Christian <77143206+tazarchriss@users.noreply.github.com>
2022-06-01 23:19:02 +0300

    Create footer.php

---
+++ include/footer.php
@@ -0,0 +1,21 @@
+<!-- This is the footer file -->
+<div id="layoutAuthentication_footer">
+                <footer class="py-4 bg-dark mt-auto">
+                    <div class="container-fluid px-4">
+                        <div class="text-center">
+                            <div class="text-muted">Copyright &copy; <i class="text-primary"> TazarChriss </i> 2022</div>
+                            
+                        </div>
+                    </div>
+                </footer>
+            </div>
+        </div>
+        <script src="js/boostrap.bundle.min.js" crossorigin="anonymous"></script>
+        <script src="js/scripts.js"></script>
+        <script src="js/chart.min.js" crossorigin="anonymous"></script>
+        <script src="assets/demo/chart-area-demo.js"></script>
+        <script src="assets/demo/chart-bar-demo.js"></script>
+        <script src="js/datatables.js" crossorigin="anonymous"></script>
+        <script src="js/datatables-simple-demo.js"></script>
+    </body>
+</html>
