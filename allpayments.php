411c9dd1e1290c35865d53ad2c04325d8ae25d1b
Baltazar Christian <77143206+tazarchriss@users.noreply.github.com>
2022-06-01 23:39:26 +0300

    Create allpayments.php

---
+++ allpayments.php
@@ -0,0 +1,102 @@
+<!-- This is the page that views all loans that have not been paid off -->
+<?php
+  session_start();
+  include 'config/connection.php';
+  if ($_SESSION['id']=='') {
+    header('Location:index.php');
+  }
+  else{
+   
+    $sql1="SELECT * FROM payment,loan,customer 
+    WHERE loan.loan_id=payment.loan
+    AND customer.cust_id=loan.customer";
+    $qry1=mysqli_query($conn,$sql1);
+    
+    include 'include/header.php'; ?>
+        <div id="layoutSidenav">
+        <?php 
+        
+        if ($_SESSION['role']=='1') {
+        include 'include/adminnav.php'; 
+        }
+        else{
+            include 'include/staffnav.php'; 
+        }
+        
+        ?>
+            <div id="layoutSidenav_content">
+                <main>
+                    <div class="container-fluid px-4 mt-2">
+                        <ol class="breadcrumb mb-4 text-right">
+                            <li class="breadcrumb-item active"><i class="fa fa-download"></i> All Payments</li>
+                        </ol>
+                     
+                        
+                        <div class="card mb-4">
+                            <div class="card-header bg-dark text-secondary">
+                                <i class="fas fa-download me-1"></i>
+                                 All Payments
+                            </div>
+                            <div class="card-body">
+                           <?php     
+                            if (mysqli_num_rows($qry1) == 0){
+
+                                ?>
+                                <h4 class="text-danger text-center">Sorry, there are no loan payment made yet !</h4>
+
+                                <?php
+                            }
+                            else{ ?>
+                                <table id="datatablesSimple">
+                                    <thead>
+                                        <tr>
+                                            <th>SN</th>
+                                            <th>Date</th>
+                                            <th>Customer</th>
+                                            <th>Amount</th>
+                                          
+                                        </tr>
+                                    </thead>
+                               
+                                   <tbody>
+                                   <?php
+                                for ($i=1; $i<=mysqli_num_rows($qry1); $i++){
+                                $row = mysqli_fetch_array($qry1);
+                           
+                                 ?>
+                                 <tr>
+                                 <td>
+                                <?php echo $i; ?>
+                                </td>
+                                <td>
+                                <h6><?php echo $row['pay_date']; ?></h6>
+                                
+                                </td>
+                                <td>
+                                <h6> <?php echo $row['fname'].' '.$row['mname'].' '.$row['lname']; ?></h6> 
+                                </td>
+                                
+                                <td>
+                                <h6><?php echo $row['amount']; ?> </h6>
+                                
+                                </td>
+  
+                                
+                            
+                              
+                                 </tr>
+
+                                   <?php 
+                                    }
+                                }
+                                    ?>
+                                   </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </main>
+                <?php include 'include/footer.php'; 
+                
+                            }
+                ?>
