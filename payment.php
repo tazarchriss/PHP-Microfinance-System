455e942b409479b0c0e8951ecc864c23e3dc56c8
Baltazar Christian <77143206+tazarchriss@users.noreply.github.com>
2022-06-02 23:12:41 +0300

    Update payment.php

--- payment.php
+++ payment.php
@@ -10,12 +10,23 @@
     $customer=$_GET['id'];
     $sql1="SELECT * FROM loan,customer 
     WHERE loan.loan_id='$customer'
-    AND customer.cust_id=loan.customer";
+    AND customer.cust_id=loan.customer
+    ORDER BY loan.issued_date DESC
+    ";
     $qry1=mysqli_query($conn,$sql1);
     
      include 'include/header.php'; ?>
         <div id="layoutSidenav">
-        <?php include 'include/adminnav.php'; ?>
+        <?php 
+        
+        if ($_SESSION['role']=='1') {
+        include 'include/adminnav.php'; 
+        }
+        else{
+            include 'include/staffnav.php'; 
+        }
+        
+        ?>
             <div id="layoutSidenav_content">
                 <main>
                     <div class="container-fluid px-4 mt-2">
@@ -60,38 +71,40 @@
                                 <div class="col-md-12">
                                 <h5>Customer: <?php echo $row['fname'].' '.$row['mname'].' '.$row['lname']; ?></h5>
                                 <hr>
-                                <h5>Issued Date: <?php echo $row['issued_date']; ?></h5>
+                                <h6>Issued Date: <?php echo $row['issued_date']; ?></h6>
                                 <hr>
-                                <h5>Issued Amount: <?php echo $row['issued_amount']; ?> Tsh</h5>
+                                <h6>Issued Amount: <?php echo $row['issued_amount']; ?> Tsh</h6>
                                 <hr>
-                                <h5>Paid Amount: <?php echo $row['paid_amount']; ?> Tsh</h5>
+                                <h6>Paid Amount: <?php echo $row['paid_amount']; ?> Tsh</h6>
                                 <hr>
-                                <h5>Remaining Amount: <?php echo $row['remain']; ?> Tsh</h5>
+                                <h6>Remaining Amount: <?php echo $row['remain']; ?> Tsh</h6>
                                 <hr>
-                                <h5>Deadline: <?php echo $row['pay_date']; ?> </h5>
+                                <h6>Fine Amount: <?php echo $row['fine']; ?> Tsh</h6>
+                                <hr>
+                                <h6>Deadline: <?php echo $row['pay_date']; ?> </h6>
                                 <hr>
                                 <input type="text" name="loan" value="<?php echo $row['loan_id']; ?>" hidden> 
                                 <input type="text" name="staff" value="<?php echo $_SESSION['id']; ?>" hidden>
                                 </div>   
                             </div>
+                           <?php if ($row['remain']>0) {
+                                            ?>
                                        
                                             <div class="row mb-3">
                                                 <div class="col-md-12">
                                                     <div class="form-floating mb-3 mb-md-0">
-                                                        <input type="text" class="form-control" name="Amount">
+                                                        <input type="text" class="form-control" name="amount">
                                                         <label for="inputFirstName">Loan Payment Amount</label>
                                                     </div>
                                                 </div>
                                                 
                                                
                                             </div>
-                                       
-                                         
-    
+                                  
     
                                             <div class="mt-4 mb-0">
                                                 <div class="d-grid">
-                                                    <button type="submit" name="save" class="btn btn-primary btn-block">Save Payment</button>
+                                                    <button type="submit" name="pay" class="btn btn-outline-primary btn-block"><i class="fa fa-save"></i> Save Payment</button>
                                                     
                                                 </div>
                                             </div>
@@ -99,6 +112,7 @@
 
                                    <?php 
                                     }
+                                }
                         
                                 }
                                     ?>
